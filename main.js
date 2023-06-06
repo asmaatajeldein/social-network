96676a0070cbbe74ecf403befc55e4c0a2681a3d
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-06 21:05:35 +0300

    added the error handling configurations

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const express = require("express");
 
 const app = express();
+const errorHandler = require("express-async-error").Handler;
 
 // imports
 require("./db");
 
+// parsing incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// error handler over any async function
+app.use(errorHandler());
+
 app.use((req, res, next) => {
   res.send("<h1 style='text-align:center'>Hello World</h1>");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).send({
+    status: statusCode,
+    message: err?.message || "Internal Server Error!",
+    errors: err?.errors || [],
+  });
 });
 
 app.listen(8000);
