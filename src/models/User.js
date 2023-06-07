31ff0ab176c80d7466a52a233f34d5d2c9bbbe60
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-07 17:39:15 +0300

    added the users model

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  user_name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const User = mongoose.model("users", userSchema);
+module.exports = User;
