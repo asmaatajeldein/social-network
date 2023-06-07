6aa255f1a3bcc21d495ee801dd715253819cf80c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-07 18:10:12 +0300

    prepared the users controller functions

---
+++ src/controllers/authenticationController.js
@@ -0,0 +1,32 @@
+const getAllUsers = (req, res, next) => {
+  res.send({ message: "All users returned!" });
+};
+
+const getUserById = (req, res, next) => {
+  res.send({ message: "the specific user returned!" });
+};
+
+const register = (req, res, next) => {
+  res.send({ message: "user created successfully!" });
+};
+
+const updateUser = (req, res, next) => {
+  res.send({ message: "user updated successfully!" });
+};
+
+const deleteUser = (req, res, next) => {
+  res.send({ message: "user deleted successfully!" });
+};
+
+const login = (req, res, next) => {
+  res.send({ message: "user logged in successfully!" });
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  register,
+  updateUser,
+  deleteUser,
+  login,
+};
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  getAllUsers,
+  getUserById,
+  register,
+  updateUser,
+  deleteUser,
+  login,
+} = require("../controllers/authenticationController");
+
 // getting all users
-router.get("/", () => {});
+router.get("/", getAllUsers);
 
 // getting user by id
-router.get("/:id", () => {});
+router.get("/:id", getUserById);
 
 // create a new user (register)
-router.post("/", () => {});
+router.post("/", register);
 
 // update user
-router.patch("/:id", () => {});
+router.patch("/:id", updateUser);
 
 // delete user
-router.delete("/:id", () => {});
+router.delete("/:id", deleteUser);
 
 // login
-router.post("/login", () => {});
+router.post("/login", login);
 
 module.exports = router;
