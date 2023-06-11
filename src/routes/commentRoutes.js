61aeb68a0e00b829eb805fabd4828c3a311a8d2c
asmaatajeldein <asmaatajeldein@gmail.com>
2023-06-12 00:07:36 +0300

    update comment routes

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const commentValidation = require("../utils/commentValidation");
+
 const {
   createComment,
-  getComments
+  getComments,
+  updateComment,
+  deleteComment
 } = require("../controllers/commentController");
 
 // create comment for a post (req must include user._id & post._id)
-router.post("/", createComment);
+router.post("/", commentValidation, createComment);
 
 // get comments for a post (req must include post._id)
 router.get("/", getComments);
 
 // update a comment in a post (must provide comment body in req & comment id in params)
-router.put("/:id", getComments);
+router.put("/:id", commentValidation, updateComment);
 
 // delete a comment from a post (must provide comment id in params)
-router.delete("/:id", getComments);
+router.delete("/:id", commentValidation, deleteComment);
 
 module.exports = router;
