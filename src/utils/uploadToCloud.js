5a92bdede1bd1ed56535b7f847075bd1140e3efc
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-06-12 21:10:44 +0300

    changed configuration and fixed uploading issues

--- src/utils/uploadToCloud.js
+++ src/utils/uploadToCloud.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 
 const cloudinary = require("cloudinary").v2;
-cloudinary.url(process.env.CLOUDINARY_URL);
+cloudinary.config({
+	cloud_name:process.env.CLOUD_NAME,
+	api_key:process.env.API_KEY,
+	api_secret:process.env.API_SECRET
+})
+// cloudinary.url(process.env.CLOUDINARY_URL);
 
 
-const uploadToCloud = async (path) => {
-	return await cloudinary.v2.uploader.upload(path);
+const uploadToCloud = (path, opt) => {
+	return cloudinary.uploader.upload(path,opt);
 };
 
 module.exports = uploadToCloud;
