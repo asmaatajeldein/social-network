ebbf29a28ab4f1556a799144176488fe0b4a1262
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-14 19:51:59 +0300

    filtered the types of the profile-pics being uploaded

--- src/utils/uploadPhotos/multer.js
+++ src/utils/uploadPhotos/multer.js
@@ -6,6 +6,19 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    // prevent upload!
+    cb({ message: "Unsupported file format!" }, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 },
+  fileFilter: fileFilter,
+});
 
 module.exports = upload;
